4d8f16d24eb7f58191ebb8d959073726876d622b
Hnatiak <romanhnatiak@gmail.com>
2023-02-18 12:39:19 +0100

    toggle
    I added size = 1200px for js

--- src/js/toggle.js
+++ src/js/toggle.js
@@ -5,19 +5,21 @@ var colorState = "white";
 		var element_light = document.getElementById("dark");
 		var element_dark = document.getElementById("light");
 
-		if (colorState === "white") {
-			body.style.backgroundColor = "#2E2E2E";
-			body.style.color = "white";
-			element.style.color = "white";
-			colorState = "#2E2E2E";
-			element_dark.style.color = "#5F6775";
-			element_light.style.color = "#4B48DB";
-		} else {
-			body.style.backgroundColor = "white";
-			body.style.color = "black";
-			element.style.color = "black";
-			colorState = "white";
-			element_dark.style.color = "#4B48DB";
-			element_light.style.color = "#5F6775";
-		}
+		if (window.innerWidth <= 1200) {
+			if (colorState === "white") {
+				body.style.backgroundColor = "#2E2E2E";
+				body.style.color = "white";
+				element.style.color = "white";
+				colorState = "#2E2E2E";
+				element_dark.style.color = "#5F6775";
+				element_light.style.color = "#4B48DB";
+			} else {
+				body.style.backgroundColor = "white";
+				body.style.color = "black";
+				element.style.color = "black";
+				colorState = "white";
+				element_dark.style.color = "#4B48DB";
+				element_light.style.color = "#5F6775";
+			}
+	}
 	}
