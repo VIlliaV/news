6a47134c3c92c6fcd7338c23ec1ce6ad25862dd3
MaximKarlov <111450676+MaximKarlov@users.noreply.github.com>
2023-02-18 20:56:25 +0000

    Deploying to gh-pages from @ VIlliaV/news@ff2ae940ed52c45413d10228693240c9bb81816d 🚀

--- favorite.c1a08ccd.js
+++
@@ -1,2 +0,0 @@
-var liElements=document.querySelectorAll("li");liElements.forEach((function(e){e.addEventListener("click",(function(){liElements.forEach((function(e){e.classList.remove("active")})),this.classList.add("active")}))}));
-//# sourceMappingURL=favorite.c1a08ccd.js.map
