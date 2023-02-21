5d006a27c65a5f22bc50e564f5cbc98af3d1a72c
MaximKarlov <predator09071987@gmail.com>
2023-02-21 12:38:15 +0200

    Editing WORK Margo

--- src/js/home.js
+++ src/js/home.js
@@ -13,48 +13,73 @@ window.addEventListener('load', () => {
 
 function generateCardsMurkup(cardsArray) {
   return cardsArray
-    .map(({ media, title, abstract, published_date, url }) => {
-      let imgUrl = media[0]['media-metadata'][2]['url'];
-      return `<li class="favorite-cards__item">
-          <div class="favorite-cards__image-cont" href="">
-            <img
-              class="favorite-cards__img"
-              src="${imgUrl}"
-              alt="asia-businesswoman"
-            />
-            <p class="favorite-cards__job-src">Job searching</p>
-            <button class="favorite-cards__remove-btn">
-              Remove from favorite
-              <svg class="favorite-cards__heart-icon" width="32" height="32">
-                <use href="images/sprite-full.svg#heart-full"></use>
-              </svg>
-            </button>
-          </div>
-          <h2 class="favorite-cards__news-title">
-          ${title}
-          </h2>
-          <p class="favorite-cards__dicription">
-            ${textCropping(abstract)}
-          </p>
-          <div class="favorite-cards__bottom">
-            <p class="favorite-cards__date">${published_date}</p>
-            <a class="favorite-cards__link" href="${url}">Read more</a>
-          </div>
-        </li>`;
-    })
+    .map(
+      item => `<li class="favorite-cards__item">
+        <a class="favorite-cards__image-link" href="${item.url}">
+          <img
+            class="favorite-cards__img"
+            src="${item.media.map(el => el['media-metadata'][2].url)}"
+            alt="${item.per_facet}"
+          />
+          <p class="favorite-cards__category">${addDefaultText(
+            item.subsection
+          )}</p>
+          <button type="button" class="favorite-cards__remove-btn id="remove-btn">
+            Remove from favorite
+            <svg class="favorite-cards__heart-icon" width="32" height="32">
+              <use href="/sprite-full.e7f74a66.svg#heart-full"></use>
+            </svg>
+          </button>
+        </a>
+        <h2 class="favorite-cards__news-title">${item.title}
+        </h2>
+        <p class="favorite-cards__dicription">
+        ${limitText(item.abstract)}
+        </p>
+        <div class="favorite-cards__bottom">
+          <p class="favorite-cards__date">${reformatDate(
+            item.published_date
+          )}</p>
+          <a class="favorite-cards__link" href="${item.url}">
+            Read more
+          </a>
+        </div>
+      </li>`
+    )
     .join('');
 }
 
-function textCropping(text) {
-  let result;
-  if (text.length > 150) {
-    text = text.slice(0, 150);
-    result = text + '...';
+function addDefaultText(text) {
+  if (text) {
+    return text;
   } else {
-    result = text;
+    return 'Other...';
   }
-  return result;
 }
+
+function limitText(text) {
+  if (text.length > 110) {
+    return text.substring(0, 110) + '...';
+  } else {
+    return text;
+  }
+}
+function reformatDate(dateString) {
+  const [year, month, day] = dateString.split('-');
+  const newDate = `${day}/${month}/${year}`;
+  return newDate;
+}
+
+// function textCropping(text) {
+//   let result;
+//   if (text.length > 150) {
+//     text = text.slice(0, 150);
+//     result = text + '...';
+//   } else {
+//     result = text;
+//   }
+//   return result;
+// }
 
 function appendCardsMurkup(cards) {
   newsCards.insertAdjacentHTML('beforeend', generateCardsMurkup(cards));
