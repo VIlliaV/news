c02b7aeb39b7c624783b96dad4a6a472719568f5
Alexandr Glazunov <matveychuk1998@gmail.com>
2023-02-21 08:08:48 +0200

    add read.js

--- src/js/open_window.js
+++ src/js/open_window.js
@@ -1,6 +1,7 @@
 const openButton = document.querySelector('.js-open-menu');
 const closeButton = document.querySelector('.js-close-menu');
 const menuContainer = document.querySelector('.js-menu-container');
+const newsList = document.querySelector('.list-news');
 
 openButton.addEventListener('click', () => {
   menuContainer.classList.remove('hidden');
@@ -11,3 +12,4 @@ closeButton.addEventListener('click', () => {
   menuContainer.classList.add('hidden');
   document.body.style.overflow = 'auto';
 });
+export { newsList };
