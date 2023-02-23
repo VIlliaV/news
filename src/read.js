fefd3d0d1561f7543f2083f35a21304ee8a29d4c
Alexandr Glazunov <matveychuk1998@gmail.com>
2023-02-23 09:18:50 +0200

    add section read

--- src/read.js
+++ src/read.js
@@ -3,4 +3,6 @@ import './js/toggle';
 import './js/toggle_tel';
 import './js/open_window';
 import './js/toggle_tel';
-import './js/read';
+import './js/read_hero';
+import './js/readMore';
+import './js/read_more_date';
