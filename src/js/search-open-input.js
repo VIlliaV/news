7cb93cfefe57a447f8591401cec6f290c50a87ad
YuriiKabo <yourrocksmirnov@gmail.com>
2023-02-24 00:50:57 +0200

    fix dark-theme and header

--- src/js/search-open-input.js
+++ src/js/search-open-input.js
@@ -1,5 +1,6 @@
 const searchIcon = document.querySelector('.search');
 const searchBox = document.querySelector('.search-box');
+// searchIcon.classList.remove('search-icon-move-wight');
 
 searchIcon.addEventListener('click', () => {
   searchBox.classList.toggle('active-input');
